757eb4ce43b7c224ddfdf3325c23f9aba4baf86c
milamber86 <beranek@icewarp.cz>
2017-11-02 03:15:20 +0100

    Update iw_fs_optimize.sh

--- iw_fs_optimize.sh
+++ iw_fs_optimize.sh
@@ -6,9 +6,15 @@
 #	- moves NDRs and other garbage mail to subdirectory
 #	- moves older mail to subfolders on folder per year basis
 #
-# variables
+### ### variables ###
+#
 # ( script options are -d: starting directory, -s: max allowed files count for directory )
 #
+# option defaults
+declare START_DIR="";
+declare MAX_SIZE="";
+#
+# get command line options
 while getopts d:s option
 do
   case "${option}"
@@ -18,12 +24,9 @@ do
   esac
 done
 #
-# functions
+### ### functions ###
 #
-#
-#
-# fill DOMAINS array with domain paths, input is full path ending with /
-#
+## fill DOMAINS array with domain paths, input is full path ending with /
 get_domain_path()
 {
 declare -a DOMAINS=( $(find "${START_DIR}" -maxdepth 1 -type d | tail -n +2) );
@@ -33,9 +36,7 @@ return 0
 }
 #
 #
-#
-# returns user paths for DOMAINS and load them into USERS array  
-#
+## returns user paths for DOMAINS and load them into USERS array  
 get_user_path()
 {
 declare local element;
@@ -49,9 +50,15 @@ return 0
 }
 #
 #
+## get file count for given path, input is full path ending with /, returns file count
+get_folder_stats()
+{
+find "${1}" -maxdepth 1 -type f | wc -l
+return 0
+}
 #
-# get file count for user maildir folders of given path, input is full path ending with /
 #
+## get file count for user maildir folders of given path, input is full path ending with /, returns maildir stats
 get_user_stats()
 {
 FPATH="${1}";
@@ -62,20 +69,28 @@ done
 return 0
 }
 #
-# 
 #
-# append path for optimize to OPTIMIZE array, input is full path ending with /
-append_for_optimize()
+## append path for optimize to end of OPTIMIZE array, input is full path ending with /
+append_to_optimize()
 {
 declare -a OPTIMIZE+=( ${1} );
-(>&2 echo ${#OPTIMIZE[@]});
+(>&2 echo ${OPTIMIZE[@]});
 (>&2 echo ${#OPTIMIZE[@]});
 return 0
 }
 #
 #
+## remove last element of OPTIMIZE array
+remove_from_optimize()
+{
+OPTIMIZE=("${OPTIMIZE[@]::${#OPTIMIZE[@]}-1}");
+(>&2 echo ${OPTIMIZE[@]});
+(>&2 echo ${#OPTIMIZE[@]});
+return 0
+} 
 #
-# process all records in USERS, append those over file limit to OPTIMIZE array
+#
+## process all records in USERS, append those over file limit to OPTIMIZE array
 search_for_optimize()
 {
 declare local element;
@@ -85,7 +100,7 @@ do
   for path in $(get_user_stats ${USERS[element]})
   do
     declare local -i size=$(echo "${path}" | sed -r s'|^([[:digit:]]+) (\/.*)$|\1|')
-    if [ "${size}" -gt "${MAX_SIZE}" ]
+    if [ "${size}" -ge "${MAX_SIZE}" ]
     then
       declare local appendpath=$(echo "${path}" | sed -r s'|^([[:digit:]]+) (\/.*)$|\2|')
       append_for_optimize "${appendpath}"
@@ -95,7 +110,33 @@ done
 return 0
 }
 #
-# MAIN
+#
+## deduplicate files in given path using fdupes, input is full path ending with /, returns new number of messages
+optimize_dupes()
+{
+(>&2 ${FDUPES} -fNI "${1}");
+get_folder_stats "${1}"
+return 0
+}
+#
+#
+## search for NDR and other garbage mail, move it to ${GARBAGE_FOLDER}, input is full path ending with /, returns new number of messages
+optimize_garbage()
+{
 
-# remove last element from array
-# array=("${array[@]::${#array[@]}-1}");
+get_folder_stats "${1}"
+return 0
+}
+#
+#
+## move older mail to subfolders, input is full path ending with /, returns new number of messages
+optimize_old()
+{
+
+get_folder_stats "${1}"
+return 0
+}
+#
+#
+### ### MAIN ###
+
