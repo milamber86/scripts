47814a78acb7ffdad045ee25b468a3d347756bb9
milamber86 <beranek@icewarp.cz>
2017-11-07 22:42:45 +0100

    Update iw_fs_optimize.sh

--- iw_fs_optimize.sh
+++ iw_fs_optimize.sh
@@ -21,8 +21,8 @@ then
 fi
 #
 # option defaults
-declare START_DIR="/mnt/data/mail/";
-declare -i MAX_SIZE=10000;
+declare START_DIR="/mnt/data/mail/"
+declare -i MAX_SIZE=1000;
 #
 # get command line options
 while getopts d:s: option
@@ -37,16 +37,14 @@ done
 ### ### functions ###
 #
 ## returns domain paths
-get_domain_path()
-{
-echo "$(find "${START_DIR}" -maxdepth 1 -type d | tail -n +2)"
+get_domain_path() {
+echo "$(find "${START_DIR}" -maxdepth 1 -type d | egrep -v "_incoming|_outgoing" | tail -n +2)"
 return 0
 }
 #
 #
 ## returns user paths for all records from DOMAINS  
-get_user_path()
-{
+get_user_path() {
 for element in $(seq 0 $((${#DOMAINS[@]} -1))) 
 do
   echo "$(find "${DOMAINS[element]}" -maxdepth 1 -type d | tail -n +2)"
@@ -56,70 +54,76 @@ return 0
 #
 #
 ## returns file count for given path, input is full path ending with /
-get_folder_stats()
-{
+get_folder_stats() {
 echo "$(find "${1}" -maxdepth 1 -type f | wc -l)"
 return 0
 }
 #
 #
 ## returns subfolder paths over file limit, input is full path ending with /
-get_user_stats()
-{
+get_user_stats() {
 IFS=$'\n'
 for path in $(find "${1}" -type d | tail -n +2 | xargs -I '{}' bash -c 'echo -e "$(find "{}" -maxdepth 1 -type f | wc -l)" "{}"')
 do
   declare -i dirsize=$(echo "${path}" | sed -r s'|^([[:digit:]]+) (\/.*)$|\1|')
   if [ "${dirsize}" -ge "${MAX_SIZE}" ]
     then
-    echo "${path}"
+      echo "${path}"
   fi
 done
 return 0
 }
 #
 #
-## deduplicate files in given path using fdupes, input is full path ending with /, returns new number of messages
-optimize_dupes()
-{
-(>&2 ${FDUPES} -fNI "${1}");
-get_folder_stats "${1}"
-return 0
-}
-#
-#
-## search for NDR and other garbage mail, move it to ${GARBAGE_FOLDER}, input is full path ending with /, returns new number of messages
-# TODO optimize search string
-optimize_garbage()
-{
->&2 find "${1}" -maxdepth 1 -type f -name "*.imap" | xargs egrep -l "^Subject: Returned mail" | xargs -I {} mv -v "{}" "${1}""${GARBAGE_FOLDER}"
-get_folder_stats "${1}"
-return 0
-}
-#
-#
-## move older mail to subfolders, input is full path ending with /, returns new number of messages
-optimize_old()
-{
-for year in {2016 2015 2014 2013 2012 2011 2010 2009 2008 2007 2006 2005 2004 2003 2002 2001 2000}
+# run optimize on all OPTIMIZE paths
+optimize() {
+IFS=$'\n'
+for element in $(seq 0 $((${#OPTIMIZE[@]} -1)))
 do
-  mkdir -p "${1}"${year}
-  cp "${1}"imapindex.dat "${1}"${year}/imapindex.dat
-  cp "${1}"imapindex.bin "${1}"${year}/imapindex.bin
-  echo "*" > "${1}"${year}/flagsext.dat
-  >&2 find "${1}" -maxdepth 1 -type f -name "${year}*.imap" | xargs -I {} mv -v "{}" "${1}"${year}
+declare -i dirsize=$(echo "${OPTIMIZE[element]}" | sed -r s'|^([[:digit:]]+) (\/.*)$|\1|')
+declare -i path=$(echo "${OPTIMIZE[element]}" | sed -r s'|^([[:digit:]]+) (\/.*)$|\2|')
+>&2 echo "Running optimize for path ${path} with ${dirsize} messages"
+>&2 echo "Deduplicate .."
+>&2 ${FDUPES} -fNI "${path}" # run deduplicate folder using fdupes -fNI
+declare -i dirsize=$(get_folder_stats "${path}")
+>&2 echo "${path} - ${dirsize}"
+read -n 1 -s -r -p "Press any key to continue" # debug
+if [ ${dirsize} -ge ${MAX_SIZE} ] # if the folder still contains more than MAX_SIZE messages, run garbage cleanup
+  then
+  >&2 echo "Garbage cleanup .."
+  declare +i MAX_SIZE="${MAX_SIZE}"
+  >&2 find "${path}" -maxdepth 1 -type f -name "*.imap" | xargs egrep -l "^Subject: Returned mail" | xargs -I {} mv -v "{}" "${path}"/"${GARBAGE_FOLDER}"_${MAX_SIZE}
+  declare -i dirsize=$(get_folder_stats "${path}")
+  >&2 echo "${path} - ${dirsize}"
+  read -n 1 -s -r -p "Press any key to continue" # debug
+  if [ ${dirsize} -ge ${MAX_SIZE} ] # if the folder still contains more than MAX_SIZE messages, run old mail cleanup
+    then
+    >&2 echo "Old mail cleanup .."
+    for year in { "2016" "2015" "2014" "2013" "2012" "2011" "2010" "2009" "2008" "2007" "2006" "2005" "2004" "2003" "2002" "2001" "2000" }
+      do
+      mkdir -p "${path}"/${year}_${MAX_SIZE}
+      cp "${path}"/imapindex.dat "${path}"/${year}_${MAX_SIZE}/imapindex.dat
+      cp "${path}"/imapindex.bin "${path}"/${year}_${MAX_SIZE}/imapindex.bin
+      echo "*" > "${path}"/${year}_${MAX_SIZE}/flagsext.dat
+      >&2 find "${path}" -maxdepth 1 -type f -name "${year}*.imap" | xargs -I {} mv -v "{}" "${path}"${year}_${MAX_SIZE}
+      done
+  read -n 1 -s -r -p "Press any key to continue" # debug
+  mkdir -p "${path}"/1999_${MAX_SIZE}
+  cp "${path}"/imapindex.dat "${path}"/1999_${MAX_SIZE}/imapindex.dat
+  cp "${path}"/imapindex.bin "${path}"/1999_${MAX_SIZE}/imapindex.bin
+  echo "*" > "${path}"/1999_${MAX_SIZE}/flagsext.dat
+  >&2 find "${path}" -maxdepth 1 -type f -name "19*.imap" | xargs -I {} mv -v "{}" "${path}"1999_${MAX_SIZE}
+  declare -i dirsize=$(get_folder_stats "${path}")
+  >&2 echo "${path} - ${dirsize}"
+  read -n 1 -s -r -p "Press any key to continue" # debug
+  fi
+fi  
 done
-mkdir -p "${1}"1999_older
-cp "${1}"imapindex.dat "${1}"1999_older/imapindex.dat
-cp "${1}"imapindex.bin "${1}"1999_older/imapindex.bin
-echo "*" > "${1}"1999_older/flagsext.dat
->&2 find "${1}" -maxdepth 1 -type f -name "${19}*.imap" | xargs -I {} mv -v "{}" "${1}"1999_older
-get_folder_stats "${1}"
 return 0
 }
-#
-#
+
 ### ### MAIN ### ( fill DOMAINS, USERS, OPTIMIZE arrays and performe optimize on paths in OTPIMIZE )
+: <<'END'
 for element in $(get_domain_path)
 do
   declare -a DOMAINS=( "${DOMAINS[@]}" "${element}" )
@@ -140,24 +144,16 @@ for element in $(seq 0 $((${#USERS[@]} -1)))
 do
   unset tmparr
   readarray tmparr <<< "$(get_user_stats "${USERS[element]}")"
-  if [ "${tmparr[@]}" != "" ] 
-   then 
+  if [ "$(echo -ne ${tmparr} | wc -m)" -ne 0 ] 
+   then
    declare -a OPTIMIZE=( "${OPTIMIZE[@]}" "${tmparr[@]}" )
   fi
 done
+END
+readarray OPTIMIZE < /root/opt_all.txt # debug
   echo "OPTIMIZE all - ${OPTIMIZE[@]}" # debug
   echo "OPTIMIZE last added - ${OPTIMIZE[-1]}" # debug
   echo "OPTIMIZE elements count - ${#OPTIMIZE[@]}" # debug
   read -n 1 -s -r -p "Press any key to continue" # debug
-for element in $(seq 0 $((${#OPTIMIZE[@]} - 1)))
-do
-  echo "Running optimize for path: ${OPTIMIZE[element]}"
-  declare -i nodupes=$(echo $(optimize_dupes ${OPTIMIZE[element]}))
-  echo "${nodupes} files left in the folder after dedup ..."
-  declare -i nogarbage=$(echo $(optimize_garbage ${OPTIMIZE[element]}))
-  echo "${nogarbage} files left in the folder after garbage cleanup ..."
-  declare -i noold=$(echo $(optimize_old ${OPTIMIZE[element]}))
-  echo "${noold} files left in the folder after old mail cleanup ..."
-  echo "Work done for ${OPTIMIZE[element]}"
-done
+optimize
 exit 0
