40f84137c702178ebe33e74399d6b92fd1e07e1c
milamber86 <beranek@icewarp.cz>
2017-12-06 12:06:30 +0100

    Update icewarp_db_xtrabackup.sh

--- icewarp_db_xtrabackup.sh
+++ icewarp_db_xtrabackup.sh
@@ -4,13 +4,19 @@
 # yum -y install http://www.percona.com/downloads/percona-release/redhat/0.1-4/percona-release-0.1-4.noarch.rpm
 # yum -y install percona-xtrabackup-24
 #!/bin/bash
-logpath="/root/dbbackup"
-backuppath="/root/dbbackup"
+backuppath="/mnt/backup/icewarp"
 thisdate="$(date +%Y%m%d-%H%M)"
-dbuser="user"
+dbuser="root"
 dbpass="password"
 mkdir -p ${backuppath}
-innobackupex --no-lock --user=${dbuser} --password=${dbpass} --stream=tar /tmp/ 2>> ${logpath}/${thisdate}.log | gzip -c | cat > ${backuppath}/bck_mysql_${thisdate}.tar.gz
-find ${backuppath}/ -type f -name "bck_*" -mtime +3 -delete > ${logpath}/${thisdate}.log 2>&1
-find ${backuppath}/ -type f -name "*.log" -mtime +30 -delete > ${logpath}/${thisdate}.log 2>&1
+innobackupex --no-lock --user=${dbuser} --password=${dbpass} --stream=tar /tmp/ | gzip -c | cat > ${backuppath}/bck_xtra_mysql_${thisdate}.tar.gz
+mysqldump --single-transaction -u ${dbuser} -p${dbpass} accounts | gzip -c | cat > ${backuppath}/bck_${thisdate}_db_acc.sql.gz
+mysqldump --single-transaction -u ${dbuser} -p${dbpass} antispam | gzip -c | cat > ${backuppath}/bck_${thisdate}_db_asp.sql.gz
+mysqldump --single-transaction -u ${dbuser} -p${dbpass} groupware | gzip -c | cat > ${backuppath}/bck_${thisdate}_db_grw.sql.gz
+mysqldump --single-transaction -u ${dbuser} -p${dbpass} webclient | gzip -c | cat > ${backuppath}/bck_${thisdate}_db_wc.sql.gz
+mysqldump --single-transaction -u ${dbuser} -p${dbpass} eas | gzip -c | cat > ${backuppath}/bck_${thisdate}_db_eas.sql.gz
+mysqldump --single-transaction -u ${dbuser} -p${dbpass} dircache | gzip -c | cat > ${backuppath}/bck_${thisdate}_db_dc.sql.gz
+tar -czf ${backuppath}/bck_${thisdate}_cf_cnf.tgz /opt/icewarp/config > /dev/null 2>&1
+tar -czf ${backuppath}/bck_${thisdate}_cf_cal.tgz /opt/icewarp/calendar > /dev/null 2>&1
+find ${backuppath}/ -type f -name "bck_*" -mtime +7 -delete > ${logpath}/${thisdate}.log 2>&1
 exit 0
