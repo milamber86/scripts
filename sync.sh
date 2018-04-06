379282cc3e5be911ae688c3694985f96cefc7e1a
milamber86 <beranek@icewarp.cz>
2018-04-06 16:36:59 +0200

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-LOG_DIR="/opt/icewarp/sync/log";
+LOG_DIR="/opt/icewarp/IMAPSYNC/log";
 mkdir -p ${LOG_DIR}
 LOG_FILE="`date "+%d-%m-%y"`.log";
 GENLOG="${LOG_DIR}/general.log"
-HOST1="81.89.48.173";
-HOST2="127.0.0.1";
+HOST1="source_host_ip";
+HOST2="destination_host_ip";
 MAXFORKS="16"
 SLINT="1";
 { while IFS=';' read u1 p1 u2 p2
@@ -12,20 +12,29 @@ do
 { echo "$u1" | egrep "^#" ; } > /dev/null && continue # this skip commented lines in sourcefile
 # max number of spawned forks controll
 while true
-        do
-        CFORKS="$(pgrep -f imapsync | wc -l)";
+	do
+       	CFORKS="$(pgrep -f "/usr/bin/imapsync" | wc -l)";
         if [ "${CFORKS}" -ge "${MAXFORKS}" ]
                                         then
                                          sleep ${SLINT}
                                         else
                                          break
         fi
-        done
+	done
 LOGTO="${LOG_DIR}/${u1}_${LOG_FILE}";
 echo "==== Syncing user $u1 to user $u2 ====" >> ${GENLOG} 2>&1
 imapsync --host1 "$HOST1" --user1 "$u1" --password1 "$p1" --host2 "$HOST2" --user2 "$u2" --password2 "$p2" \
+--regextrans2 's/Sent Items$/Odeslan&AOk-/' \
+--regextrans2 's/Deleted Items$/Ko&AWE-/' \
+--regextrans2 's/Drafts$/Koncepty/' \
+--regextrans2 's/Junk E-mail$/Ko&AWE-/' \
+--regextrans2 's/Deleted Messages$/Ko&AWE-/' \
+--regextrans2 's/Infected Items$/Ko&AWE-/' \
+--regextrans2 's/AVG Virov&AP0- trezor$/Ko&AWE-/' \
+--regextrans2 's/Infikovan&AOk- polo&AX4-ky$/Ko&AWE-/' \
+--exclude '^Public Folders' \
 --syncinternaldates --addheader --noauthmd5 >> ${LOGTO} 2>&1 &
 done
-} < file1.txt
-wait
+} < file.txt
+wait ${!}
 exit 0
