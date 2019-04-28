ec55ae8e558771c1f5b817a12bdc61b9f9d375d9
milamber86 <beranek@icewarp.cz>
2019-04-28 02:36:05 +0200

    Create gwdelete.php

---
+++ gwdelete.php<? define('SHAREDLIB_PATH',get_cfg_var('icewarp_sharedlib_path')); require_once(SHAREDLIB_PATH.'system.php'); slSystem::import('api/gw'); $api = IceWarpAPI::instance('tool/groupware/gwdelete.php
@@ -0,0 +1,11 @@
+<?
+define('SHAREDLIB_PATH',get_cfg_var('icewarp_sharedlib_path'));
+require_once(SHAREDLIB_PATH.'system.php');
+slSystem::import('api/gw');
+$api = IceWarpAPI::instance('tool/groupware/export');
+$gwapi = new IceWarpGWAPI();
+$gwapi->user = $api->GetProperty('C_GW_SuperUser');
+$gwapi->pass = $api->GetProperty('C_GW_SuperPass');
+$gwapi->Login();
+echo $gwapi->FunctionCall("DeleteOwner",$gwapi->sessid,$argv[1],1);
+?>
