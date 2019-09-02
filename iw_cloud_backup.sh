beffd5b1bdd74447d01a53ea43e39f226680e043
milamber86 <beranek@icewarp.cz>
2019-09-02 10:29:45 +0200

    Update iw_cloud_backup.sh

--- iw_cloud_backup.sh
+++ iw_cloud_backup.sh
@@ -1,31 +1,72 @@
 #!/bin/bash
-backuppath="/mnt/data/backup"
-mkdir -p /mnt/data
+source /etc/icewarp/icewarp.conf
+maildirpath="$(${IWS_INSTALL_DIR}/tool.sh get system C_System_Storage_Dir_MailPath | grep -P '(?<=: ).*(?=/mail/)' -o)"
+backuppath="${maildirpath}/backup"
+scriptdir="$(cd $(dirname $0) && pwd)"
+mkdir -p ${maildirpath}
 mkdir -p ${backuppath}
+mkdir -p ${scriptdir}/logs
 backupsrvhost="${1}" # if we take backups from another host
 backupsrvport="${2}" # than the one we connect to
-test="$(find /usr/lib64 -type f -name "Entities.pm")"
-if [[ -z "${test}" ]]
+cloudplan=$(${IWS_INSTALL_DIR}/tool.sh get system c_license_xml | grep -P '(?<=<cloudplanislive>).*(?=</cloudplanislive>)' -o -m 1)
+logdate="$(date +%Y%m%d)"
+logfile="${scriptdir}/logs/bck_${logdate}.log"
+
+function log {
+  echo $(date +%H:%M:%S) $1 >> ${logfile}
+}
+
+log "Starting backup."
+
+if [ -z "$cloudplan" ] || [ "$cloudplan" != "1" ]
+then
+  log "Aborting - no LIVE Cloudplan."
+  exit 0
+fi
+
+fstabtest=$(/usr/bin/cat /etc/fstab | grep -o "${maildirpath}")
+mounttest=$(/usr/bin/mount | grep -o "${maildirpath}")
+if [[ "$maildirpath" == "$fstabtest" ]]
+then
+  log "Storage is a mountpoint - checking."
+  if [[ "$maildirpath" != "$mounttest" ]]
+    then
+    log "Storage not mounted. Aborting."
+    exit 0
+  else
+    log "Storage mountcheck ok."
+  fi
+else
+  log "Storage is not a mountpoint - continuing."
+fi
+
+utiltest="$(/usr/bin/find /usr/lib64 -type f -name "Entities.pm")"
+if [[ -z "${utiltest}" ]]
   then
+  log "Installing Entities.pm"
   /usr/bin/yum -y install epel-release
   /usr/bin/yum -y install perl-HTML-Encoding.noarch
 fi
-dbpass="$(cat /opt/icewarp/config/_webmail/server.xml | cat /opt/icewarp/config/_webmail/server.xml | egrep -o "<dbpass>.*</dbpass>" | perl -pe 's|^\<dbpass\>(.*)\</dbpass\>$|\1|' | perl -MHTML::Entities -pe 'decode_entities($_);')"
-wcdbuser="$(cat /opt/icewarp/config/_webmail/server.xml | egrep -o "<dbuser>.*</dbuser>" | perl -pe 's|^\<dbuser\>(.*)\</dbuser\>$|\1|')"
-read -r dbhost dbport wcdbname <<< $(cat /opt/icewarp/config/_webmail/server.xml | egrep -o "<dbconn>mysql:host=.*;port=.*;dbname=.*</dbconn>" | perl -pe 's|^\<dbconn\>mysql:host=(.*);port=(.*);dbname=(.*)\</dbconn\>$|\1 \2 \3|')
-read -r accdbname accdbuser <<< $(/opt/icewarp/tool.sh get system c_system_storage_accounts_odbcconnstring | perl -pe 's|^c_system_storage_accounts_odbcconnstring: (.*);(.*);.*;.*;.*;.*$|\1 \2|')
-read -r aspdbname aspdbuser <<< $(/opt/icewarp/tool.sh get system c_as_challenge_connectionstring | perl -pe 's|^c_as_challenge_connectionstring: (.*);(.*);.*;.*;.*;.*$|\1 \2|')
-read -r grwdbname grwdbuser <<< $(/opt/icewarp/tool.sh get system c_gw_connectionstring | perl -pe 's|^c_gw_connectionstring: (.*);(.*);.*;.*;.*;.*$|\1 \2|')
-read -r dcdbname dcdbuser <<< $(/opt/icewarp/tool.sh get system c_accounts_global_accounts_directorycacheconnectionstring | perl -pe 's|^c_accounts_global_accounts_directorycacheconnectionstring: (.*);(.*);.*;.*;.*;.*$|\1 \2|')
-easdbname="$(/opt/icewarp/tool.sh get system c_activesync_dbconnection | /opt/icewarp/tool.sh get system c_activesync_dbconnection | egrep -o "mysql:host=.*;dbname=.*" | perl -pe 's|^mysql:host=.*;port=.*;dbname=(.*)$|\1|')"
-easdbuser="$(/opt/icewarp/tool.sh get system c_activesync_dbuser | perl -pe 's|^c_activesync_dbuser: (.*)$|\1|')"
-easdbpass="$(/opt/icewarp/tool.sh get system c_activesync_dbpass | perl -pe 's|^c_activesync_dbpass: (.*)$|\1|')"
+log "Reading DB access data."
+dbpass="$(/usr/bin/cat ${IWS_INSTALL_DIR}/config/_webmail/server.xml | cat ${IWS_INSTALL_DIR}/config/_webmail/server.xml | egrep -o "<dbpass>.*</dbpass>" | perl -pe 's|^\<dbpass\>(.*)\</dbpass\>$|\1|' | perl -MHTML::Entities -pe 'decode_entities($_);')"
+wcdbuser="$(/usr/bin/cat ${IWS_INSTALL_DIR}/config/_webmail/server.xml | egrep -o "<dbuser>.*</dbuser>" | perl -pe 's|^\<dbuser\>(.*)\</dbuser\>$|\1|')"
+read -r dbhost dbport wcdbname <<< $(cat ${IWS_INSTALL_DIR}/config/_webmail/server.xml | egrep -o "<dbconn>mysql:host=.*;port=.*;dbname=.*</dbconn>" | perl -pe 's|^\<dbconn\>mysql:host=(.*);port=(.*);dbname=(.*)\</dbconn\>$|\1 \2 \3|')
+read -r accdbname accdbuser <<< $(${IWS_INSTALL_DIR}/tool.sh get system c_system_storage_accounts_odbcconnstring | perl -pe 's|^c_system_storage_accounts_odbcconnstring: (.*);(.*);.*;.*;.*;.*$|\1 \2|')
+read -r aspdbname aspdbuser <<< $(${IWS_INSTALL_DIR}/tool.sh get system c_as_challenge_connectionstring | perl -pe 's|^c_as_challenge_connectionstring: (.*);(.*);.*;.*;.*;.*$|\1 \2|')
+read -r grwdbname grwdbuser <<< $(${IWS_INSTALL_DIR}/tool.sh get system c_gw_connectionstring | perl -pe 's|^c_gw_connectionstring: (.*);(.*);.*;.*;.*;.*$|\1 \2|')
+read -r dcdbname dcdbuser <<< $(${IWS_INSTALL_DIR}/tool.sh get system c_accounts_global_accounts_directorycacheconnectionstring | perl -pe 's|^c_accounts_global_accounts_directorycacheconnectionstring: (.*);(.*);.*;.*;.*;.*$|\1 \2|')
+easdbname="$(${IWS_INSTALL_DIR}/tool.sh get system c_activesync_dbconnection | ${IWS_INSTALL_DIR}/tool.sh get system c_activesync_dbconnection | egrep -o "mysql:host=.*;dbname=.*" | perl -pe 's|^mysql:host=.*;port=.*;dbname=(.*)$|\1|')"
+easdbuser="$(${IWS_INSTALL_DIR}/tool.sh get system c_activesync_dbuser | perl -pe 's|^c_activesync_dbuser: (.*)$|\1|')"
+easdbpass="$(${IWS_INSTALL_DIR}/tool.sh get system c_activesync_dbpass | perl -pe 's|^c_activesync_dbpass: (.*)$|\1|')"
 if [ -z "${easdbpass}" ]; then easdbpass="easdbpass not discovered"; fi
 if [[ "${dbpass}" =~ "^sqlite:.*" ]]; then dbpass="${easdbpass}"; fi
 if [ -z "${dbpass}" ]; then dbpass="dbpass not discovered"; fi
 if [ ! -z "${backupsrvhost}" ]; then dbhost="${backupsrvhost}"; fi # if we take backups from another host
 if [ ! -z "${backupsrvport}" ]; then dbport="${backupsrvport}"; fi # than the one we connect to
-if [[ "${accdbuser}" = *"DBUIWC"* ]]
+
+dbcheck=$(/usr/bin/mysql -u ${accdbuser} -p${dbpass} -h ${dbhost} -P ${dbport} -e 'SHOW DATABASES;' | grep -cw "${accdbname}\|${grwdbname}\|${aspdbname}\|${dcdbname}\|${easdbname}\|${wcdbname}")
+log "Starting DB backup."
+if [[ "${dbcheck}" == 3 ]]
 then # generic_cloud ( DBUIWC*, DBUIWC*EAS, DBUIWC*WC)
 /usr/bin/mysqldump --single-transaction -u ${accdbuser} -p${dbpass} -h${dbhost} -P ${dbport} ${accdbname} | gzip -c | cat > ${backuppath}/bck_db_acc_asp_grw_dc_${accdbname}`date +%Y%m%d-%H%M`.sql.gz &
 /usr/bin/mysqldump --single-transaction -u ${easdbuser} -p${easdbpass} -h${dbhost} -P ${dbport} ${easdbname} | gzip -c | cat > ${backuppath}/bck_db_eas_${easdbname}`date +%Y%m%d-%H%M`.sql.gz &
@@ -38,10 +79,16 @@ else # non-generic cloud ( other db name settings )
 /usr/bin/mysqldump --single-transaction -u ${easdbuser} -p${easdbpass} -h${dbhost} -P ${dbport} ${easdbname} | gzip -c | cat > ${backuppath}/bck_db_eas_${easdbname}`date +%Y%m%d-%H%M`.sql.gz &
 /usr/bin/mysqldump --single-transaction -u ${wcdbuser} -p${dbpass} -h${dbhost} -P ${dbport} ${wcdbname} | gzip -c | cat > ${backuppath}/bck_db_wc_${wcdbname}`date +%Y%m%d-%H%M`.sql.gz &
 fi
+log "Finished DB backup."
 wait ${!}
-tar -czf ${backuppath}/bck_cnf`date +%Y%m%d-%H%M`.tgz /opt/icewarp/config > /dev/null 2>&1
-tar -czf ${backuppath}/bck_cal`date +%Y%m%d-%H%M`.tgz /opt/icewarp/calendar > /dev/null 2>&1
-/opt/icewarp/tool.sh export account "*@*" u_backup > ${backuppath}/bck_acc_backup`date +%Y%m%d-%H%M`.csv
-/opt/icewarp/tool.sh export domain "*" d_backup > ${backuppath}/bck_dom_backup`date +%Y%m%d-%H%M`.csv
-find ${backuppath}/ -type f -name "bck_*" -mtime +3 -delete > /dev/null 2>&1
+log "Starting IW config backup."
+/usr/bin/tar -czf ${backuppath}/bck_cnf`date +%Y%m%d-%H%M`.tgz ${IWS_INSTALL_DIR}/config > /dev/null 2>&1
+/usr/bin/tar -czf ${backuppath}/bck_cal`date +%Y%m%d-%H%M`.tgz ${IWS_INSTALL_DIR}/calendar > /dev/null 2>&1
+${IWS_INSTALL_DIR}/tool.sh export account "*@*" u_backup > ${backuppath}/bck_acc_backup`date +%Y%m%d-%H%M`.csv
+${IWS_INSTALL_DIR}/tool.sh export domain "*" d_backup > ${backuppath}/bck_dom_backup`date +%Y%m%d-%H%M`.csv
+log "Finished IW config backup."
+log "Cleaning old backups and logs."
+/usr/bin/find ${backuppath}/ -type f -name "bck_*" -mtime +3 -delete > /dev/null 2>&1
+/usr/bin/find ${scriptdir}/logs/ -type f -name "bck_*.log" -mtime +30 -delete > /dev/null 2>&1
+log "All done."
 exit 0
