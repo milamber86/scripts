bd87b8aaf7c69a8dd286531ac514d63f2d213b3b
milamber86 <beranek@icewarp.cz>
2019-10-30 23:12:57 +0100

    Update domainmove.sh

--- domainmove.sh
+++ domainmove.sh
@@ -12,24 +12,26 @@ grwdbname=groupware
 mkdir -p ${targetpath}
 { while IFS=';' read email
         do
-	      username=$(/opt/icewarp/tool.sh export account ${email} u_mailbox | awk -F ',' '{print $2}');
+	username=$(/opt/icewarp/tool.sh export account ${email} u_mailbox | awk -F ',' '{print $2}');
         from=$(/opt/icewarp/tool.sh export account "${email}" u_fullmailboxpath | awk -F ',' '{print $2}' | sed -r "s|${sourcepath}/(.*)/|${sourcepath}/\1|");
         to=$(/opt/icewarp/tool.sh export account "${email}" u_fullmailboxpath | awk -F ',' '{print $2}' | sed -r "s|${sourcepath}/(.*)/|${targetpath}/\1|");
         if [ -d "${to}" ]; then
-                             	echo "Not moving ${from}, target path ${to} already exists!"
-                              rc1=1
+                             echo "Not moving ${from}, target path ${to} already exists!"
+                             rc1=1
                            else
-                     	       	mv -v ${from} ${to}
-                              rc1=$?
+                     	     mv -v ${from} ${to}
+                             rc1=$?
                        	   fi
         if [ $rc1 -eq 0 ]; then
-                               	userid=$(echo -e "use ${accdbname};SELECT U_ID FROM Users WHERE U_Mailbox = \x27${username}\x27 AND U_Domain = \x27${sourcedom}\x27;" | mysql | grep -v U_ID);
-                                echo -e "use ${accdbname};UPDATE Users SET U_Domain = \x27${targetdom}\x27 WHERE U_ID = ${userid};" | mysql
-                                echo -e "use ${accdbname};UPDATE Aliases SET A_Domain = \x27${targetdom}\x27 WHERE A_UserID = ${userid}" | mysql
-                               	gwownid=$(echo -e "use ${grwdbname};SELECT OWN_ID FROM EventOwner WHERE OWN_Email = \x27${email}\x27;" | mysql | grep -v OWN_ID | awk '{print $1}');
-                               	newemail=$(echo -e "${email}" | sed -r "s|${sourcedom}|${targetdom}|");
-                                echo -e "use ${grwdbname};UPDATE EventOwner SET OWN_Email = \x27${newemail}\x27 WHERE OWN_ID = \x27${gwownid}\x27" | mysql
-                               	sed -i -r "s|${sourcedom}|${targetdom}|g" "${to}/~webmail/settings.xml"
+                             userid=$(echo -e "use ${accdbname};SELECT U_ID FROM Users WHERE U_Mailbox = \x27${username}\x27 AND U_Domain = \x27${sourcedom}\x27;" | mysql | grep -v U_ID);
+                             echo -e "use ${accdbname};UPDATE Users SET U_Domain = \x27${targetdom}\x27 WHERE U_ID = ${userid};" | mysql
+                             echo -e "use ${accdbname};UPDATE Aliases SET A_Domain = \x27${targetdom}\x27 WHERE A_UserID = ${userid}" | mysql
+                             gwownid=$(echo -e "use ${grwdbname};SELECT OWN_ID FROM EventOwner WHERE OWN_Email = \x27${email}\x27;" | mysql | grep -v OWN_ID | awk '{print $1}');
+                             newemail=$(echo -e "${email}" | sed -r "s|${sourcedom}|${targetdom}|");
+                             echo -e "use ${grwdbname};UPDATE EventOwner SET OWN_Email = \x27${newemail}\x27 WHERE OWN_ID = \x27${gwownid}\x27" | mysql
+			     			 newmpath=$(/opt/icewarp/tool.sh export account "${newemail}" u_mailboxpath | awk -F ',' '{print $2}' | sed -r "s|${sourcedom}|${targetdom}|")
+							 /opt/icewarp/tool.sh set account "${newemail}" u_mailboxpath "${newmpath}"
+							 sed -i -r "s|${sourcedom}|${targetdom}|g" "${to}/~webmail/settings.xml"
                        	    fi
         done
 } < ${userlist}
