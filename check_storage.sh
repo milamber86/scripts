4c9a336ad21cba29acea74857f2ba2c47e2fc0e8
milamber86 <beranek@icewarp.cz>
2019-12-02 00:57:29 +0100

    Update check_storage.sh

--- check_storage.sh
+++ check_storage.sh
@@ -10,7 +10,7 @@ for pid in $(pgrep -f check_storage.sh); do
 	if [ ${pid} != $$ ]; then
 	 echo "$(date) - Another check already running, killing PID ${pid}, sending alert to ${monitoring}, killing IceWarp services." > ${logfile} 2>&1
 	 kill -9 ${pid}
-#    	 /opt/icewarp/icewarpd.sh --stop all >> ${logfile}
+    	 /opt/icewarp/icewarpd.sh --stop all >> ${logfile}
 	fi
 done
 if [ -f "${storagepath}"/storage.dat ]
