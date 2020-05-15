441758a9cd12dadedde0b704d53cc16862f7ca9f
milamber86 <beranek@icewarp.cz>
2020-05-15 21:32:41 +0200

    Update fix_imapindex.sh

--- fix_imapindex.sh
+++ fix_imapindex.sh
@@ -130,7 +130,6 @@ local dbQuery="$(echo -e "delete from item where folder_id = ${folderDbId};")";
 local dbResult="$(echo "${dbQuery}" | mysql "${dbName}")";
 # todo test db result
 local dbQuery="$(echo -e "update folder set uid_validity=NULL, sync_update=0 where folder_id = ${folderDbId};")";
-#local dbQuery="$(echo -e "update folder set uid_validity=NULL, sync_update=0 where folder_id = ${folderDbId};")";
 local dbResult="$(echo "${dbQuery}" | mysql "${dbName}")";
 # todo test db result
 }
@@ -162,23 +161,23 @@ local pass="${2}";
 # get auth token
 local atoken_request="<iq uid=\"1\" format=\"text/xml\"><query xmlns=\"admin:iq:rpc\" ><commandname>getauthtoken</commandname><commandparams><email>${email}</email><password>${pass}</password><digest></digest><authtype>0</authtype><persistentlogin>0</persistentlogin></commandparams></query></iq>"
 local wcatoken="$(curl -s --connect-timeout ${ctimeout} -m ${ctimeout} -ikL --data-binary "${atoken_request}" "https://${iwserver}/icewarpapi/" | egrep -o "<authtoken>(.*)</authtoken>" | sed -r s'|<authtoken>(.*)</authtoken>|\1|')"
-##echo "1: ${wcatoken}"
+ ## echo "1: ${wcatoken}"
 # get phpsessid
 local wcphpsessid="$(curl -s --connect-timeout ${ctimeout} -m ${ctimeout} -ikL "https://${iwserver}/webmail/?atoken=$( rawurlencode "${wcatoken}" )" | egrep -o "PHPSESSID_LOGIN=(.*); path=" | sed -r 's|PHPSESSID_LOGIN=wm(.*)\; path=|\1|' | head -1 | tr -d '\n')"
-##echo "2: ${wcphpsessid}"
+ ## echo "2: ${wcphpsessid}"
 # auth wc session
 local auth_request="<iq type=\"set\"><query xmlns=\"webmail:iq:auth\"><session>wm"${wcphpsessid}"</session></query></iq>"
 local wcsid="$(curl -s --connect-timeout ${ctimeout} -m ${ctimeout} -ikL --data-binary "${auth_request}" "https://${iwserver}/webmail/server/webmail.php" | egrep -o 'iq sid="(.*)" type=' | sed -r s'|iq sid="wm-(.*)" type=|\1|')";
-##echo "3: ${wcsid}"
+## echo "3: ${wcsid}"
 
 # refresh folders standard account start
 local refreshfolder_request="<iq sid=\"wm-"${wcsid}"\" uid=\"${email}\" type=\"set\" format=\"xml\"><query xmlns=\"webmail:iq:accounts\"><account action=\"refresh\" uid=\"${email}\"/></query></iq>"
 local response="$(curl -s --connect-timeout ${ctimeout} -m ${ctimeout} -ikL --data-binary "${refreshfolder_request}" "https://${iwserver}/webmail/server/webmail.php")"
-##echo "4: ${response}"
+ ## echo "4: ${response}"
 # folder refresh
 local refreshfolder_request="<iq sid=\"wm-"${wcsid}"\" uid=\"734012818541404900158955161135\" type=\"get\" format=\"xml\"><query xmlns=\"webmail:iq:items\"><account uid=\"${email}\"><folder uid=\"${folderName}\"><item><values><subject/><to/><sms/><from/><date/><size/><flags/><has_attachment/><color/><priority/><smime_status/><item_moved/><tags/><ctz>120</ctz></values><filter><limit>68</limit><offset>0</offset><sort><date>desc</date><item_id>desc</item_id></sort></filter></item></folder></account></query></iq>"
 local response="$(curl -s --connect-timeout ${ctimeout} -m ${ctimeout} -ikL --data-binary "${refreshfolder_request}" "https://${iwserver}/webmail/server/webmail.php")"
-##echo "5: ${response}"
+ ## echo "5: ${response}"
 # session logout
 local logout_request="<iq sid=\"wm-"${wcsid}"\" type=\"set\"><query xmlns=\"webmail:iq:auth\"/></iq>"
 curl -s --connect-timeout ${ctimeout} -m ${ctimeout} -ikL --data-binary "${logout_request}" "https://${iwserver}/webmail/server/webmail.php" > /dev/null 2>&1
@@ -242,27 +241,34 @@ for i in "${imapFolders[@]}"
 do
   cmdResult=$(testImapFolder "${1}" "${2}" "${i}");
   if [[ ${?} -ne 0 ]] ; then
-    echo "FAIL IMAP - User: ${1}, folder: ${i}, fullpath: ${cmdResult}."
- #   echo "-----------------------------------------------"
+    echo "FAIL IMAP - User: ${1}, folder: ${i}, fullpath: ${cmdResult}. Trying to repair."
     prepFolderRestore "${1}" "${cmdResult}"
           else
- ##   echo "   OK IMAP - User: ${1}, ${cmdResult} msgs, folder: ${i}."
-    imapCnt=${cmdResult};
-    cmdResult="$(testWcFolder "${1}" "${i}")";
-    if [[ $cmdResult -ne ${imapCnt} ]] ; then
-    echo "FAIL WC - User: ${1}, folder: ${i}, wc cache/imap have: ${cmdResult} / ${imapCnt} msgs.";
-#    echo "-----------------------------------------------"
-    fixWcFolder "${1}" "${i}";
-    refreshWcFolder "${1}" "${2}" "${i}";
-    cmdResult=$(testImapFolder "${1}" "${2}" "${i}");
-    echo "Status after repair: folder: ${i}, wc cache have: ${cmdResult} msgs.";
-      else
-      sleep 0
-##      echo "   OK WC - User: ${1}, ${cmdResult} msgs, folder: ${i}."
-    fi
+           ##   echo "   OK IMAP - User: ${1}, ${cmdResult} msgs, folder: ${i}."
+          continue;
   fi
 done
-#if [[ -f "${tmpFile}" ]]; then
-#  indexFix
-#fi
+if [[ -f "${tmpFile}" ]]; then
+  indexFix
+fi
+for i in "${imapFolders[@]}"
+do
+  cmdResult=$(testImapFolder "${1}" "${2}" "${i}");
+  if [[ ${?} -ne 0 ]] ; then
+  echo "FAIL IMAP 2nd time - User: ${1}, folder: ${i}, fullpath: ${cmdResult}. Giving up."
+  continue
+          else
+          imapCnt=${cmdResult};
+          cmdResult="$(testWcFolder "${1}" "${i}")";
+          if [[ $cmdResult -ne ${imapCnt} ]] ; then
+          echo "FAIL WC - User: ${1}, folder: ${i}, wc cache/imap have: ${cmdResult} / ${imapCnt} msgs.";
+          fixWcFolder "${1}" "${i}";
+          refreshWcFolder "${1}" "${2}" "${i}";
+          cmdResult=$(testImapFolder "${1}" "${2}" "${i}");
+          echo "Status after repair: folder: ${i}, wc cache have: ${cmdResult} msgs.";
+                      else
+                       ##      echo "   OK WC - User: ${1}, ${cmdResult} msgs, folder: ${i}."
+                      continue;
+          fi
+done
 exit 0
