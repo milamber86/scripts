685e7f9c99d1191ca710202f24ea74349835c3e1
milamber86 <beranek@icewarp.cz>
2020-05-27 01:30:32 +0200

    Update iw_export_gw_webclient.sh

--- iw_export_gw_webclient.sh
+++ iw_export_gw_webclient.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-iwserver="127.0.0.1";		 					# IceWarp server IP/host
-email="user@server"; 							# email address, standard user must exist, guest user will be created by this script if it does not exist
-pass="pass; 	        						# password
-declare -i guest=0;			    			# test account type, 0 - standard user account, 1 - teamchat guest account
-ctimeout="50";             				# curl connection timeout in seconds
+iwserver="127.0.0.1";                             # IceWarp server IP/host
+declare -i guest=0;                               # test account type, 0 - standard user account, 1 - teamchat guest account
+ctimeout="50";                                    # curl connection timeout in seconds
+tmpFile="/root/tmpFile";
+tmpFolders="/root/tmpFolders";
+exportPath="/root/exporttst";
+mkdir -p "${exportPath}"
 
-# urlencode string function
-rawurlencode() {
+function rawurlencode # urlencode string function
+{
   local string="${1}"
   local strlen=${#string}
   local encoded=""
   local pos c o
-
   for (( pos=0 ; pos<strlen ; pos++ )); do
      c=${string:$pos:1}
      case "$c" in
@@ -23,35 +24,99 @@ rawurlencode() {
   echo "${encoded}"
 }
 
-# todo: list all grupware folders for the account
-
-start=`date +%s%N | cut -b1-13`
-# get auth token
+function sessionLogin # ( 1: user@email, 2: password -> wcSid - webclient session ID )
+{
+email="${1}";
+pass="${2}";
 atoken_request="<iq uid=\"1\" format=\"text/xml\"><query xmlns=\"admin:iq:rpc\" ><commandname>getauthtoken</commandname><commandparams><email>${email}</email><password>${pass}</password><digest></digest><authtype>0</authtype><persistentlogin>0</persistentlogin></commandparams></query></iq>"
-wcatoken="$(curl --connect-timeout ${ctimeout} -m ${ctimeout} -ikL --data-binary "${atoken_request}" "https://${iwserver}/icewarpapi/" | egrep -o "<authtoken>(.*)</authtoken>" | sed -r s'|<authtoken>(.*)</authtoken>|\1|')"
-
+wcatoken="$(curl --connect-timeout ${ctimeout} -m ${ctimeout} -kL --data-binary "${atoken_request}" "https://${iwserver}/icewarpapi/" | egrep -o "<authtoken>(.*)</authtoken>" | sed -r s'|<authtoken>(.*)</authtoken>|\1|')"
 # get phpsessid
 wcphpsessid="$(curl --connect-timeout ${ctimeout} -m ${ctimeout} -ikL "https://${iwserver}/webmail/?atoken=$( rawurlencode "${wcatoken}" )" | egrep -o "PHPSESSID_LOGIN=(.*); path=" | sed -r 's|PHPSESSID_LOGIN=wm(.*)\; path=|\1|' | head -1 | tr -d '\n')"
-
 # auth wc session
 auth_request="<iq type=\"set\"><query xmlns=\"webmail:iq:auth\"><session>wm"${wcphpsessid}"</session></query></iq>"
-wcsid="$(curl --connect-timeout ${ctimeout} -m ${ctimeout} -ikL --data-binary "${auth_request}" "https://${iwserver}/webmail/server/webmail.php" | egrep -o 'iq sid="(.*)" type=' | sed -r s'|iq sid="wm-(.*)" type=|\1|')";
+wcSid="$(curl --connect-timeout ${ctimeout} -m ${ctimeout} -kL --data-binary "${auth_request}" "https://${iwserver}/webmail/server/webmail.php" | egrep -o 'iq sid="(.*)" type=' | sed -r s'|iq sid="wm-(.*)" type=|\1|')";
+echo "${wcSid}";
+}
 
+function sessionLogout # ( 1: wcSid )
+{
+wcSid="${1}";
+logout_request="<iq sid=\"wm-"${wcSid}"\" type=\"set\"><query xmlns=\"webmail:iq:auth\"/></iq>"
+logout="$(curl --connect-timeout ${ctimeout} -m ${ctimeout} -kL --data-binary "${logout_request}" "https://${iwserver}/webmail/server/webmail.php")";
+}
+
+function exportFolderVCF # ( 1: wcSid, 2: user@email, 3: folderName, 4: targetPath )
+{
+wcSid=${1};
+email="${2}";
+folderName="${3}";
+targetPath="${4}";
+start=`date +%s%N | cut -b1-13`
 # export Contacts folder to vcf format
 encemail="$(rawurlencode "${email}")"
-curl --connect-timeout ${ctimeout} -m ${ctimeout} -kL "https://${iwserver}/webmail/server/download.php?sid=wm-${wcsid}&class=exportvcard&fullpath=${encemail}%2FContacts" > "${email}.Contacts.vcf"
-
-# export Calendar folder to ics format
-calexport_request="<iq sid=\"wm-${wcsid}\" type=\"set\"><query xmlns=\"webmail:iq:folders\"><account uid=\"${email}\"><folder uid=\"Calendar\" action=\"save_items\"/></account></query></iq>"
-calexport="$(curl --connect-timeout ${ctimeout} -m ${ctimeout} -kL --data-binary "${calexport_request}" "https://${iwserver}/webmail/server/webmail.php" | egrep -o "<fullpath>(.*)</fullpath>" | perl -pe 's|<fullpath>(.*)</fullpath>|\1|')"
-curl --connect-timeout ${ctimeout} -m ${ctimeout} -ikL "https://${iwserver}/webmail/server/download.php?class=file&fullpath=${calexport}&sid=wm-${wcsid}" > "${email}.Calendar.vcf"
-
-# todo: export Tasks, Notes, Documents
-
-# session logout
-logout_request="<iq sid=\"wm-"${wcsid}"\" type=\"set\"><query xmlns=\"webmail:iq:auth\"/></iq>"
-curl --connect-timeout ${ctimeout} -m ${ctimeout} -ikL --data-binary "${logout_request}" "https://${iwserver}/webmail/server/webmail.php"
-
+curl --connect-timeout ${ctimeout} -m ${ctimeout} -kL "https://${iwserver}/webmail/server/download.php?sid=wm-${wcSid}&class=exportvcard&fullpath=${encemail}%2F${folderName}" > "${targetPath}"
 end=`date +%s%N | cut -b1-13`
 runtime=$((end-start))
+echo "${runtime};${targetPath}";
+}
+
+function exportFolderICS # ( 1: wcSid, 2: user@email, 3: folderName, 4: targetPath )
+{
+wcSid=${1};
+email="${2}";
+folderName="${3}";
+targetPath="${4}";
+start=`date +%s%N | cut -b1-13`
+# export Calendar folder to ics format
+calexport_request="<iq sid=\"wm-${wcSid}\" type=\"set\"><query xmlns=\"webmail:iq:folders\"><account uid=\"${email}\"><folder uid=\"${folderName}\" action=\"save_items\"/></account></query></iq>"
+calexport="$(curl --connect-timeout ${ctimeout} -m ${ctimeout} -kL --data-binary "${calexport_request}" "https://${iwserver}/webmail/server/webmail.php" | egrep -o "<fullpath>(.*)</fullpath>" | perl -pe 's|<fullpath>(.*)</fullpath>|\1|')"
+curl --connect-timeout ${ctimeout} -m ${ctimeout} -ikL "https://${iwserver}/webmail/server/download.php?class=file&fullpath=${calexport}&sid=wm-${wcSid}" > "${targetPath}"
+end=`date +%s%N | cut -b1-13`
+runtime=$((end-start))
+echo "${runtime};${targetPath}";
+}
+
+function getFolders # ( 1: wcSid, 2: user@email -> gw folders list )
+{
+wcSid="${1}";
+email="${2}";
+start=`date +%s%N | cut -b1-13`
+getfolder_request="<iq sid=\"wm-${wcSid}\" uid=\"${email}\" type=\"set\" format=\"json\"><query xmlns=\"webmail:iq:accounts\"><account action=\"refresh\" uid=\"${email}\"/></query></iq>";
+getfolder_response="$(curl --connect-timeout ${ctimeout} -m ${ctimeout} -kL --data-binary "${getfolder_request}" "https://${iwserver}/webmail/server/webmail.php")";
+end=`date +%s%N | cut -b1-13`
+runtime=$((end-start))
+echo "${getfolder_response}" | egrep -o '\"TYPE\":\[\{\"VALUE\":\"[[:alnum:]]\"\}\]|\"RELATIVE_PATH\":\[\{\"VALUE\":\"[[:alnum:]\\ \/]*\"\}\]' | tr -d '[]{}' | sed -r 's|"VALUE":||' > "${tmpFile}"
+}
+
+function parseFolders # ( 1: folder list from getFolders -> folder_name;type )
+{
+local wantType=0;
+while IFS=':' read attr value; do
+        if [[ ${attr} =~ '"RELATIVE_PATH"' ]]; then
+          local folderName="${value}";
+          local wantType=1;
+          else if [[ ( ${attr} =~ '"TYPE"' ) && ( wantType -eq 1 ) ]]; then
+            if [[ ( ${value} =~ '"E"' ) || ( ${value} =~ '"C"' ) || ( ${value} =~ '"T"' ) ]]; then
+              echo "${folderName};${value};"
+              local wantType=0;
+            fi
+          fi
+        fi
+done < "${tmpFile}" > "${tmpFolders}"
+}
+
+# main
+wcSid="$(sessionLogin "${1}" "${2}")";
+getFolders "${wcSid}" "${1}";
+parseFolders
+mkdir -p "${exportPath}/${1}";
+while IFS=';' read name type; do
+  folderName="$(echo "${name}" | tr -d '"')";
+  case ${type} in
+   '"E"') exportFolderICS "${wcSid}" "${1}" "${folderName}" "${exportPath}/${1}/${folderName}.ics" ;;
+   '"C"') exportFolderVCF "${wcSid}" "${1}" "${folderName}" "${exportPath}/${1}/${folderName}.vcf" ;;
+   '"T"') exportFolderICS "${wcSid}" "${1}" "${folderName}" "${exportPath}/${1}/${folderName}.ics" ;;
+  esac
+done < "${tmpFolders}"
+sessionLogout "${wcSid}"
 exit 0
