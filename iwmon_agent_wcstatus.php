7083be1ba00307a3708383f92e315ab1fd786dfe
milamber86 <beranek@icewarp.cz>
2021-01-06 18:47:20 +0100

    php exec dependency removed

--- iwmon_agent_wcstatus.php
+++ iwmon_agent_wcstatus.php
@@ -1,5 +1,5 @@
 <?php
-    if( exec('grep OK /opt/icewarp/var/wcstatus.mon')) {
+    if( file_exists ( '/opt/icewarp/var/wcstatus.mon.OK' )) {
           exit("OK");}
     else {
           header('HTTP/1.1 500 Internal Server Error');
