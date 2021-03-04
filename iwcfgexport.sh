285dceb6936825b8ab94ce23c7235608b7879110
milamber86 <beranek@icewarp.cz>
2021-03-04 19:22:09 +0100

    Update iwcfgexport.sh

--- iwcfgexport.sh
+++ iwcfgexport.sh
@@ -6,6 +6,8 @@ gwSuperPass="$(/opt/icewarp/tool.sh get system C_GW_SuperPass | awk '{print $2}'
 accStorageMode="$(/opt/icewarp/tool.sh get system C_System_Storage_Accounts_StorageMode | awk '{print $2}')"
 mailPath="$(/opt/icewarp/tool.sh get system C_System_Storage_Dir_MailPath | awk '{print $2}')"
 archivePath="$(/opt/icewarp/tool.sh get system C_System_Tools_AutoArchive_Path | awk '{print $2}')"
+tempPath="$(/opt/icewarp/tool.sh get system C_System_Storage_Dir_TempPath | awk '{print $2}')"
+logsPath="$(/opt/icewarp/tool.sh get system C_System_Storage_Dir_LogPath | awk '{print $2}')"
 accDbConn="$(/opt/icewarp/tool.sh get system c_system_storage_accounts_odbcconnstring | awk '{print $2}')"
 dcDbConn="$(/opt/icewarp/tool.sh get system c_accounts_global_accounts_directorycacheconnectionstring | awk '{print $2}')"
 gwDbConn="$(/opt/icewarp/tool.sh get system c_gw_connectionstring | awk '{print $2}')"
@@ -13,7 +15,7 @@ asDbConn="$(/opt/icewarp/tool.sh get system c_as_challenge_connectionstring | aw
 easDbConn="$(/opt/icewarp/tool.sh get system C_ActiveSync_DBConnection | awk '{print $2}')"
 easDbUser="$(/opt/icewarp/tool.sh get system C_ActiveSync_DBUser | awk '{print $2}')"
 easDbPass="$(/opt/icewarp/tool.sh get system C_ActiveSync_DBPass | awk '{print $2}')"
-for I in orderId superPass gwSuperPass accStorageMode mailPath archivePath accDbConn dcDbConn gwDbConn asDbConn easDbConn easDbUser easDbPass;
+for I in orderId superPass gwSuperPass accStorageMode mailPath archivePath tempPath logsPath accDbConn dcDbConn gwDbConn asDbConn easDbConn easDbUser easDbPass;
   do
     eval ref=\$${I};
     echo -e "${I} ${ref}" >> ${cfgExportFile};
