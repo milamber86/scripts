50d741235a183c017e9265909ead58ee3039c286
milamber86 <beranek@icewarp.cz>
2021-06-22 10:48:27 +0200

    Update kpmon.sh

--- kpmon.sh
+++ kpmon.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
+# IceWarp healthcheck for keepalived web 
+# ( requires scripts/iwmon_agent.sh )
 reqchecks="imapstatus httpstatus wcstatus";
 wcmaxlatency=24000;
-#dbmasterstatus=$(mysql -e "SELECT @@global.read_only\G" | tail -1 | awk '{print $2}');
-#if [[ ${dbmasterstatus} -ne 0 ]]
-#  then
-#    echo "Database set to read-only, not a master IceWarp cluster instance.";
-#    rm -f /opt/icewarp/var/wcstatus.mon.OK
-#    exit 1
-#fi
+dbmasterstatus=$(mysql -e "SELECT @@global.read_only\G" | tail -1 | awk '{print $2}');
+if [[ "${dbmasterstatus}" != "0" ]]
+  then
+    echo "Database set to read-only, not a master IceWarp cluster instance.";
+    rm -f /opt/icewarp/var/wcstatus.mon.OK
+    exit 1
+fi
 for I in ${reqchecks}
   do
   grep "OK" /opt/icewarp/var/${I}.mon > /dev/null 2>&1
