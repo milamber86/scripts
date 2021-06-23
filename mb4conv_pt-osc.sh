3106dd6d38fdb6dc828a36f334d7aae84c4d325a
milamber86 <beranek@icewarp.cz>
2021-06-23 17:12:24 +0200

    Update mb4conv_pt-osc.sh

--- mb4conv_pt-osc.sh
+++ mb4conv_pt-osc.sh
@@ -8,6 +8,7 @@ dbhost="127.0.0.1";
 dbport="3306";
 charset="utf8mb4";
 collate="utf8mb4_swedish_ci";
+echo 'SET GLOBAL FOREIGN_KEY_CHECKS=0;' | mysql -h ${dbhost} -u ${dbuser} -p${dbpass} -P ${dbport}
 databases=`mysql -h ${dbhost} -u ${dbuser} -p${dbpass} -N -e "SHOW DATABASES;" | tr -d "| " | egrep -v "Database|schema|mysql|sys"`
 for db in ${databases}
   do
@@ -15,7 +16,8 @@ for db in ${databases}
     tables=$(mysql -h ${dbhost} -u ${dbuser} -p${dbpass} -P ${dbport} -N -e "SHOW TABLES FROM ${db}" | tr -d "| ")
     for table in ${tables}
       do
-        pt-online-schema-change --user=${dbuser} --password=${dbpass} --host=${dbhost} --port=${dbport} --charset ${charset} --alter "CONVERT TO CHARACTER SET ${charset} COLLATE ${collate}" D=${db},t=${table} --exec --alter-foreign-keys-method=rebuild_constraints
+        pt-online-schema-change --user=${dbuser} --password=${dbpass} --host=${dbhost} --port=${dbport} --charset ${charset} --alter "CONVERT TO CHARACTER SET ${charset} COLLATE ${collate}" D=${db},t=${table} --exec --alter-foreign-keys-method=auto
     done
 done
+echo 'SET GLOBAL FOREIGN_KEY_CHECKS=1;' | mysql -h ${dbhost} -u ${dbuser} -p${dbpass} -P ${dbport}
 exit 0
